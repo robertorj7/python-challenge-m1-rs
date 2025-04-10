3217ffdd31c074590efdb40dafd4a0caec30f6d6
Roberto Jr <rrjr87@outlook.com>
2025-04-10 16:07:15 -0300

    Criado menu

---
+++ agenda.py
@@ -0,0 +1,16 @@
+while True:
+    print("\nMenu da Agenda")
+    print("1. Adicionar Novo Contato")
+    print("2. Mostrar Lista de Contatos")
+    print("3. Editar Contato")
+    print("4. Marcar Contato como Favorito")
+    print("5. Mostrar Lista de Contatos Favoritos")
+    print("6. Apagar Contato")
+    print("7. Sair")
+
+    escolha = input("Digite a sua escolha: ")
+
+    if escolha == "7":
+        break
+    
+print("Programa Finalizado!")
